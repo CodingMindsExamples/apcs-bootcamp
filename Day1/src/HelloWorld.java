75e38fe14830ac2c7aa99c9a821beef84410e745
Yu Sun <yu.sun.cs@gmail.com>
2017-07-24 23:03:47 -0700

    Day 1

---
+++ Day1/src/HelloWorld.java
@@ -0,0 +1,24 @@
+
+// This is just a demo Java program to show the Hello World example.
+// More stuff.
+// This is written by Yu Sun.
+public class HelloWorld {
+	
+	// This is the entrance of the my java program
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I like programming in Java."); // String
+        System.out.println("This is the 3rd line of code.");
+        /*
+         * The following code shows how to do Math operations in Java.
+         * It includes addition, subtraction, multiplication, and division.
+         */
+        System.out.println(3 + 5);
+        System.out.println("10 - 15");
+        System.out.println(3 * 6);
+        System.out.println(16 / 4);  
+        System.out.println(Math.PI);  // This PI value is provided by Java
+        System.out.println(Math.PI * 3 * 3);
+    }
+    
+}
